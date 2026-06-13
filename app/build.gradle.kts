f2a2eb194162920b974102b568623f1cd0adefb8
saad-khan-rind <khansaad45678900@gmail.com>
2026-06-13 18:53:44 +0200

    Pixel playlist switch logic improvement

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
 
     defaultConfig {
         applicationId = "com.saad_khan_rind.atmosphere_effect"
-        versionName = "5.8.3"
-        versionCode = 200583
+        versionName = "5.8.4"
+        versionCode = 200584
     }
 
     flavorDimensions += "apiLevel"
@@ -20,14 +20,14 @@ android {
             dimension = "apiLevel"
             minSdk = 36
             targetSdk = 36
-            versionCode = 200583
+            versionCode = 200584
         }
 
         create("v33") {
             dimension = "apiLevel"
             minSdk = 33
             targetSdk = 33
-            versionCode = 100583
+            versionCode = 100584
         }
 
     }
