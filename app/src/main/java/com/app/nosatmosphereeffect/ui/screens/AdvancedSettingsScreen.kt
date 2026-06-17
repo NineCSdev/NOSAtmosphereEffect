42fca199b61fb298d9d86772b4cdec0fb748ea9e
saad-khan-rind <khansaad45678900@gmail.com>
2026-06-17 04:07:04 +0200

    remove extra imports

--- app/src/main/java/com/app/nosatmosphereeffect/ui/screens/AdvancedSettingsScreen.kt
+++ app/src/main/java/com/app/nosatmosphereeffect/ui/screens/AdvancedSettingsScreen.kt
@@ -2,7 +2,6 @@ package com.app.nosatmosphereeffect.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,11 +18,11 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.app.nosatmosphereeffect.R
@@ -90,7 +89,7 @@ fun AdvancedSettingsScreen(
     var poll by remember { mutableStateOf(config.poll) }
     var delay by remember { mutableStateOf(config.delay) }
     var duration by remember { mutableStateOf(config.duration) }
-    var rotationIndex by remember { mutableStateOf(config.initialRotationIndex) }
+    var rotationIndex by remember { mutableIntStateOf(config.initialRotationIndex) }
 
     var dotSize by remember { mutableFloatStateOf(config.dotSize) }
     var grayscale by remember { mutableStateOf(config.grayscale) }
